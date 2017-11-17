9f1dcf29e0e269208aaf501aad33be969c0e2206
Guest User <Guest@Epicodus-3D.local>
2017-11-17 11:26:04 -0800

    pass review specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, :presence => true
+  validates :content, :presence => true,
+            :length => { :minimum => 50, :maximum => 250 }
+  validates :rating, :presence => true
+  validates :rating, inclusion: { in: 1..5 }
+  validates :product_id, :presence => true
 end
