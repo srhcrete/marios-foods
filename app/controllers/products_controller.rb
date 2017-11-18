385044282d72de949a5787122b61081dc629d64f
Sarah Lindmar <srhcrete@gmail.com>
2017-11-17 17:05:26 -0800

    add new form for Products, fix layout/errors render for form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,17 @@ class ProductsController < ApplicationController
     @featured = Product.featured.first
     @lastThree = Product.lastThree.limit(3)
   end
+
+  def new
+    @products = Product.all
+    @newProduct = Product.new
+  end
+
+  def create
+    @products = Product.all
+    @product = Product.new(product_params)
+  end
+
 private
   def product_params
     params.require(:product).permit(:name)
