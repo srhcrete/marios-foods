2ff4020f24f5a22ee421ac3b6a9ba248a4201038
Sarah Lindmar <srhcrete@gmail.com>
2017-11-20 15:41:05 -0800

    add edit product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product details have been udpated."
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :cost, :country)
