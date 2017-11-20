101f311c10ca75f3444b8b97a039865b3af8c9c4
Sarah Lindmar <srhcrete@gmail.com>
2017-11-20 15:06:02 -0800

    fix new product form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,9 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @reviews = @product.reviews
   end
+
+private
+  def product_params
+    params.require(:review).permit(:author, :content, :rating)
+  end
 end
